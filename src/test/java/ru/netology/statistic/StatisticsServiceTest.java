d7acfe0a5d18c657f9aee01d99e095b33cbeed8b
maksim19881988 <89222634554m@gmail.com>
2024-12-15 21:57:39 +0500

    Add test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,40 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxWithNegativeNumbers() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-1, -5, -3, -2};
+        long expected = -1;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWithDuplicates() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {7, 7, 8, 8, 9, 9, 9};
+        long expected = 9;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWithSortedArray() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        long expected = 10;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
